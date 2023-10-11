c758e4e42188b93f509f81ccf2ce2fdc51813213
PrytMaks <prytkovmaks@gmail.com>
2023-10-12 00:58:39 +0500

    feat: уверенно идем к освоению React

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="footer">
+      Тут будет Footer
+    </div>
+  )
+}
+
+export default Footer;
---
+++ src/Technologies.js
@@ -0,0 +1,10 @@
+const Technologies = () => {
+  return(
+    <div className="1">
+      Тут будут технологии
+      Вперед макс , все получится 
+    </div>
+  )
+}
+
+export default Technologies;
