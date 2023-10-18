4378b7ea9f44f40ea74aeb45e56a87bad8ab94eb
PrytMaks <prytkovmaks@gmail.com>
2023-10-18 23:02:56 +0500

    feat: Добавил маршрутизацию по страницам

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
-import s from './App.module.css';
+import s from "./App.module.css";
 import Navbar from "./components/Navbar/Navbar";
-import Header from './components/Header/Header';
-//import Profile from './components/Profile/Profile';
+import Header from "./components/Header/Header";
+import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/DIalogs";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
+
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className={s.app_wrapper}>
-      <Header/>
-      <Navbar/>
-      <div className={s.app_wrapper_content}>
-        <Dialogs/>
-        {/* <Profile/> */}
+    <BrowserRouter>
+      <div className={s.app_wrapper}>
+        <Header />
+        <Navbar />
+        <div className={s.app_wrapper_content}>
+          <Routes>
+            <Route path="/profile" element={<Profile/>} />
+            <Route path="/dialogs" element={<Dialogs/>} />
+            <Route path="/news" element={<News/>} />
+            <Route path="/music" element={<Music/>} />
+            <Route path="/settings" element={<Settings/>} />
+          </Routes>
+        </div>
       </div>
-      
-     
-    </div>
+    </BrowserRouter>
   );
 };
 
