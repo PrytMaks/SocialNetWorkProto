971b1d708f073e0eb875732acdb09f5f6f6130b8
PrytMaks <prytkovmaks@gmail.com>
2023-10-19 22:34:28 +0500

    feat: добавили страницу Dialogs

--- src/components/Dialogs/DIalogs.jsx
+++ src/components/Dialogs/DIalogs.jsx
@@ -2,8 +2,26 @@ import s from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
   return (
-  <div className={s.dialog}>
-    Dialogs
+  <div className={s.dialogs}>
+    <div className={s.dialogs_items}>
+      <div className={s.dialog + ' ' + s.active}>
+        Макс
+      </div>
+      <div className={s.dialog}>
+        Андрей
+      </div>
+      <div className={s.dialog}>
+        Света
+      </div>
+      <div className={s.dialog}>
+        Вова
+      </div>
+    </div>
+    <div className={s.messages}>
+      <div className={s.message}>Hi</div>
+      <div className={s.message}>How is your IT-Kama</div>
+      <div className={s.message}>Maaaks</div>
+    </div>
   </div>
   );
 }
