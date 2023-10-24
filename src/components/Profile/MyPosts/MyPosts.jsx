b55ed594a38d344776d34227fc0db88f5a44aa5b
PrytMaks <prytkovmaks@gmail.com>
2023-10-24 20:02:14 +0500

    feat: удалил тестовые данные из пропсов, стилизовал

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,14 +2,19 @@ import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 const MyPosts = (props) => {
   return (
-    <div className="">
-      My posts
+    <div className={s.postsBlock}>
+      <h3>My posts</h3>
+      
       <div className="">
-        <textarea name="" id="" cols="30" rows="10"></textarea>
-        <button>Add post</button>
+        <div className="">
+          <textarea name="" id="" cols="30" rows="10"></textarea>
+        </div>
+        <div className="">
+          <button>Add post</button>
+        </div>
       </div>
       <div className={s.posts}>
-        <Post likes = '3' nameA = 'Макс не сдавайся все получится!!!' message = 'Hello how are you?'/>
+        <Post likes="3" message="Hello how are you?" />
       </div>
     </div>
   );
