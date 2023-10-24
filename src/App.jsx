0bd3d1739f3535544dce03cb3efe315bed8b7604
PrytMaks <prytkovmaks@gmail.com>
2023-10-24 21:40:10 +0500

    refactor: rename words errors

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import s from "./App.module.css";
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
 import Profile from "./components/Profile/Profile";
-import Dialogs from "./components/Dialogs/DIalogs";
+import Dialogs from "./components/Dialogs/Dialogs";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
