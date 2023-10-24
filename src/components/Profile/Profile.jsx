2ba42000cdf826b717c3b0d78e4137e317cd374f
PrytMaks <prytkovmaks@gmail.com>
2023-10-24 20:01:15 +0500

    feat: Вынесли посты в отдельный компонент

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,17 +1,12 @@
-import profileBg from "./profileBg.jpg";
-import s from "./Profile.module.css";
-
+//import s from "./Profile.module.css";
+import PrfolieInfo from "./ProfileInfo/ProfileInfo.jsx";
 import MyPosts from "./MyPosts/MyPosts";
+
 const Profile = () => {
   return (
     <div>
-      <div className="">
-        <img src={profileBg} alt="img" className={s.profile_bg} />
-      </div>
-      <div className={s.user_wall_wrapper}>
-        <div className="">Avatar - description</div>
-        <MyPosts />
-      </div>
+      <PrfolieInfo />
+      <MyPosts />
     </div>
   );
 };
