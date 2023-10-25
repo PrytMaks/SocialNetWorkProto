398ca3a4b507cf13a6a28c192c8ccace2c19d9c6
PrytMaks <prytkovmaks@gmail.com>
2023-10-25 23:22:18 +0500

    feat: научился взаимодействовать с createRef, считывал даныне с текстового поля

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,10 +1,19 @@
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
+import React from "react";
+
 const MyPosts = (props) => {
+
+  let newPostElement = React.createRef();
   
   let postsData = props.posts;
 
   let postsElements = postsData.map( el => <Post likesCount={el.likesCount} message={el.message} key={el.id}/>)
+  
+  const addPost = () => {
+    let text = newPostElement.current.value;
+    console.log(text)
+  }
 
   return (
     <div className={s.postsBlock}>
@@ -12,10 +21,10 @@ const MyPosts = (props) => {
   
       <div className="">
         <div className="">
-          <textarea name="" id="" cols="30" rows="10"></textarea>
+          <textarea ref={newPostElement} name="" id="" cols="30" rows="10"></textarea>
         </div>
         <div className="">
-          <button>Add post</button>
+          <button onClick={addPost}>Add post</button>
         </div>
       </div>
       <div className={s.posts}>
