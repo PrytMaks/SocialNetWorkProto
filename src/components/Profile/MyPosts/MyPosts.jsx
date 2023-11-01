cfb48a2b90a3729c9d64164aabc632be088756d2
PrytMaks <prytkovmaks@gmail.com>
2023-11-01 23:51:33 +0500

    feat: прокинул dispatch, создал объекты action для отправки в стейт

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,37 +3,43 @@ import Post from "./Post/Post";
 import React from "react";
 
 const MyPosts = (props) => {
-
   let newPostElement = React.createRef();
   let postsData = props.posts;
 
-  let postsElements = postsData.map( el => <Post likesCount={el.likesCount} message={el.message} key={el.id}/>)
-  
+  let postsElements = postsData.map((el) => (
+    <Post likesCount={el.likesCount} message={el.message} key={el.id} />
+  ));
+
   const addPost = () => {
-    props.addPost();
-  }
+    props.dispatch({ type: "ADD-POST" });
+  };
   let onPostChange = () => {
     let text = newPostElement.current.value;
-    props.updateNewPostText(text);
-  }
-  
+    let action = { type: "UPDATE-NEW-POST-TEXT", text: text };
+    props.dispatch(action);
+  };
+
   return (
     <div className={s.postsBlock}>
       <h3>My posts</h3>
-  
+
       <div className="">
         <div className="">
-          <textarea ref={newPostElement}
+          <textarea
+            ref={newPostElement}
             onChange={onPostChange}
-            name="" id="" cols="30" rows="10" value= {props.newPostText}/>
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            value={props.newPostText}
+          />
         </div>
         <div className="">
           <button onClick={addPost}>Add post</button>
         </div>
       </div>
-      <div className={s.posts}>
-        {postsElements}
-      </div>
+      <div className={s.posts}>{postsElements}</div>
     </div>
   );
 };
