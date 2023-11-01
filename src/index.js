38c97b430d81000d3fe88d4b51b6a96c84d3ffa2
PrytMaks <prytkovmaks@gmail.com>
2023-11-01 23:50:33 +0500

    feat:  прокинул dispatch + забайндил чтобы не потерять контекст

--- src/index.js
+++ src/index.js
@@ -7,10 +7,11 @@ import store from './redux/state';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 let rerenderEntireTree = () => {
+  
   root.render(
     <React.StrictMode>
-      <App state = {store.getState()} addPost = {store.addPost.bind(store)} 
-           updateNewPostText = {store.updateNewPostText.bind(store)}/>
+      <App state = {store.getState()} 
+           dispatch = {store.dispatch.bind(store)} />
     </React.StrictMode>
   );
 }
