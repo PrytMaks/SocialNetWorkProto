9cc1d06db5e99e23d1c3a2070bb6f49c986dab27
PrytMaks <prytkovmaks@gmail.com>
2023-11-07 20:50:38 +0500

    feat: добавили логику добавления сообщения на странице Dialogs;

--- src/components/Dialogs/DIalogs.jsx
+++ src/components/Dialogs/DIalogs.jsx
@@ -1,19 +1,49 @@
 import s from "./Dialogs.module.css";
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
-
+import React from "react";
+import {
+  updateNewMessageBodyCreator,
+  sendMessageCreator,
+} from "../../redux/state";
 
 const Dialogs = (props) => {
-  let dialogsElements = props.state.dialogs.map( el => <DialogItem name={el.name} id={el.id} key={el.id}/>);
-  let messagesElements = props.state.messages.map( el =><Message message={el.message} id={el.id} key={el.id}/>);
+  let state = props.store.getState().dialogsPage;
+  let newMessageBody = state.newMessageText;
+
+  let dialogsElements = state.dialogs.map((el) => (
+    <DialogItem name={el.name} id={el.id} key={el.id} />
+  ));
+  let messagesElements = state.messages.map((el) => (
+    <Message message={el.message} id={el.id} key={el.id} />
+  ));
+
+
+  let onNewMessageChange = (e) => {
+
+    let body = e.target.value;
+    props.dispatch(updateNewMessageBodyCreator(body));
+  };
+  let onSendMessageClick = () => {
+    props.dispatch(sendMessageCreator());
+  };
 
   return (
     <div className={s.dialogs}>
-      <div className={s.dialogs_items}>
-        {dialogsElements}
-      </div>
+      <div className={s.dialogs_items}>{dialogsElements}</div>
       <div className={s.messages}>
-        {messagesElements}
+        <div className="">
+          {messagesElements} 
+        </div>
+        <div className="">
+          <textarea
+            onChange={onNewMessageChange}
+            value={newMessageBody}
+          />
+        </div>
+        <div className="">
+          <button onClick={onSendMessageClick}>Send!</button>
+        </div>
       </div>
     </div>
   );
