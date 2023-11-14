3ed42a401790f760abe79ba21612bd2e061fb7c7
PrytMaks <prytkovmaks@gmail.com>
2023-11-14 20:33:48 +0500

    feat: удалили rerenderTree и subscribe, так как react-redux делает это автоматически, и более точечно(с нашей функцией перезагружались все компоненты), теперб перезагружаются только измененные!

--- src/index.js
+++ src/index.js
@@ -7,22 +7,13 @@ import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-let rerenderEntireTree = () => {
-  root.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>
-  );
-};
-
-rerenderEntireTree(store.getState());
-
-store.subscribe(() => {
-  let state = store.getState();
-  rerenderEntireTree(state);
-});
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
