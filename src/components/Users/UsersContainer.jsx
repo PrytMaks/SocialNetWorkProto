6e89fa217014458380ea2f74a43e4ab278ac88c9
PrytMaks <prytkovmaks@gmail.com>
2023-11-27 18:14:56 +0500

    refactor: убрали второрй контейнерный компонент ищ отдельного файла, и расположили в родительском (*Users container) компоненте

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,7 +1,39 @@
-
-import UsersAPIComponent from './UsersAPIComponent';
 import { followAC, unfollowAC, setUsersAC, setCurrentPageAC, setUsersTotalCountAC} from '../../redux/usersReducer';
 import { connect } from 'react-redux';
+import React from "react";
+import axios from "axios";
+import Users from './Users';
+
+class UsersAPIComponent extends React.Component {
+  componentDidMount() {
+    axios
+      .get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+        this.props.setTotalUsersCount(response.data.totalCount);
+      });
+  }
+  onPageChanged = (pageNum) => {
+    this.props.setCurrentPage(pageNum);
+    axios
+    .get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNum}&count=${this.props.currentPage.pageSize}`)
+    .then((response) => {
+      this.props.setUsers([...response.data.items]);
+    });
+  }
+  render() {
+    return <Users 
+      totalUsersCount = {this.props.totalUsersCount}
+      pageSize = {this.props.pageSize}
+      currentPage = {this.props.currentPage}
+      onPageChanged = {this.onPageChanged}
+      follow={this.props.follow}
+      unfollow = {this.props.unfollow}
+      users = {this.props.users}
+    />
+  }
+}
+
 
 const mapStateToProps = (state) => {
   return {
@@ -33,5 +65,9 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent);
+
+
+
+
 
 export default UsersContainer;
