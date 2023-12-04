86cb2d20c182c50f8c6d5f31ef38bbb83f8417bc
PrytMaks <prytkovmaks@gmail.com>
2023-12-04 20:09:21 +0500

    feat: Убрал, userId (ненужынй здеесь) в зависимостях хука, из-за циклической перезагрузки (постоянно отправлял запрос на сервер)

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -6,20 +6,20 @@ import { setAuthUserDataAC } from '../../redux/authReducer';
 
 
 const HeaderContainer = (props) => {
-  let userId = 2;
+
   useEffect( () => {
     axios
       .get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {
         withCredentials: true
       })
       .then((response) => {
-        console.log(response)
+
         if(response.data.resultCode === 0){
           let {id, login, email} = response.data.data
           props.setAuthUserDataAC(id, email, login);
         }
       });
-  }, [props, userId]);
+  }, [props]);
 
   return ( 
     <Header {...props} />
