7e5d2684bbf2771101c9b07c9f2eea66db0fc9ea
PrytMaks <prytkovmaks@gmail.com>
2023-12-04 21:37:05 +0500

    feat: исправил баг - перепутана логика follow/unfollow + ошибка в delete запросе(параметры переданы неверно) + ошибка самого адреса API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,17 +2,15 @@ import s from "./users.module.css";
 import React from "react";
 import userPhoto from "../../assets/images/usualProfile.png";
 import { NavLink } from "react-router-dom";
-import  axios from 'axios';
+import axios from "axios";
 
 const Users = (props) => {
-  //Высчитываем количество нужных нам страниц в Page count: делим общее количество всех пользователей с сервера (кол-во страниц определили сами = 5, нужно разделить общее колво пользователей на колво страниц) 
+  //Высчитываем количество нужных нам страниц в Page count: делим общее количество всех пользователей с сервера (кол-во страниц определили сами = 5, нужно разделить общее колво пользователей на колво страниц)
   //let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
   let pages = [];
   for (let i = 1; i <= 10; i++) {
     pages.push(i);
   }
-
-
 
   return (
     <div className="">
@@ -23,7 +21,7 @@ const Users = (props) => {
             onClick={(e) => {
               props.onPageChanged(page);
             }}
-            key = {page}
+            key={page}
           >
             {page}
           </span>
@@ -33,7 +31,7 @@ const Users = (props) => {
         <div key={u.id}>
           <span>
             <div className={s.photoWrapper}>
-              <NavLink to={'/profile/' + u.id}>
+              <NavLink to={"/profile/" + u.id}>
                 <img
                   src={u.photos.small != null ? u.photos.small : userPhoto}
                   alt=""
@@ -43,38 +41,50 @@ const Users = (props) => {
             </div>
             <div>
               {u.followed ? (
-                <button onClick={() => {
-
-                  axios.post(`https://social-network.samuraijs.com/api/1.0//follow${u.id}`, {}, {
-                    withCredentials: true,
-                    headers: {
-                      "API-KEY": "d41de8bb-c175-4c94-bb80-b3217e0d6207",
-                    },
-                    
-                  }).then((response) => {
-                      if(response.data.resultCode === 0) {
-                        props.unfollow(u.id)
-                      }
-                  });
-                  
-                  
-                }}>Unfollow</button>
+                <button
+                  onClick={() => {
+                    axios
+                      .delete(
+                        `https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,
+                        {
+                          withCredentials: true,
+                          headers: {
+                            "API-KEY": "d41de8bb-c175-4c94-bb80-b3217e0d6207",
+                          },
+                        }
+                      )
+                      .then((response) => {
+                        if (response.data.resultCode === 0) {
+                          props.unfollow(u.id);
+                        }
+                      });
+                  }}
+                >
+                  Unfollow
+                </button>
               ) : (
-                <button onClick={() => {
-                  axios.delete(`https://social-network.samuraijs.com/api/1.0//follow${u.id}`, {}, {
-                    withCredentials: true,
-                    headers: {
-                      "API-KEY": "d41de8bb-c175-4c94-bb80-b3217e0d6207",
-                    },
-                  }).then((response) => {
-                      if(response.data.resultCode === 0) {
-                        props.follow(u.id);
-                      }
-                  });
-
-
-                  
-                }}>Follow</button>
+                <button
+                  onClick={() => {
+                    axios
+                      .post(
+                        `https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,
+                        {},
+                        {
+                          withCredentials: true,
+                          headers: {
+                            "API-KEY": "d41de8bb-c175-4c94-bb80-b3217e0d6207",
+                          },
+                        }
+                      )
+                      .then((response) => {
+                        if (response.data.resultCode === 0) {
+                          props.follow(u.id);
+                        }
+                      });
+                  }}
+                >
+                  Follow
+                </button>
               )}
             </div>
           </span>
