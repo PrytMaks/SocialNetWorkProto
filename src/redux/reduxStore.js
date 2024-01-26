264ea17ae43055a05be5135cf578cabbe5fed388
PrytMaks <prytkovmaks@gmail.com>
2024-01-26 22:09:33 +0500

    feat: Добавили код для расширения ReduxDevTools

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, legacy_createStore as createStore } from "redux";
+import { applyMiddleware, combineReducers, compose, legacy_createStore as createStore } from "redux";
 import profileReducer from "./profileReducer";
 import dialogsReducer  from "./dialogsReducer";
 import usersReducer from "./usersReducer";
@@ -17,7 +17,8 @@ let reducers = combineReducers({
   app: appReducer,
 });
 
-let store = createStore(reducers, applyMiddleware(thunk)); 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, /* preloadedState, */ composeEnhancers(applyMiddleware(thunk)));
 
 window.store = store;
 
